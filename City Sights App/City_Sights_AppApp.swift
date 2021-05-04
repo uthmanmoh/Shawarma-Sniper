f145b15565fed7d42b866d90a8366579d8992784
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-05-04 05:53:05 -0400

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Uthman Mohamed on 2021-05-04.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City Sights App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City Sights App
+//
+//  Created by Uthman Mohamed on 2021-05-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
